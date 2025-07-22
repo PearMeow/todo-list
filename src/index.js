e44cd64cf35ecdc6db54a029fc482ac05d02e12a
Perry Huang <perryhuang2015@gmail.com>
2025-07-21 20:42:28 -0400

    Create classes for task and project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 console.log("This is some template text");
+
+// sidebar first
+// what do we want to store the projects in?
+// array probably
+// Array that contains each project
+// Project object that contains an array of todos
+
+const projects = [];
+
+class Project {
+    constructor(name) {
+        this.name = name;
+        this.tasks = [];
+    }
+
+    addTask(title, desc, dueDate, prio) {
+        this.tasks.push(Task(title, desc, dueDate, prio));
+    }
+};
+
+class Task {
+    constructor(title, desc, dueDate, prio) {
+        this.title = title;
+        this.desc = desc;
+        this.dueDate = dueDate;
+        this.prio = prio;
+    }
+}
+
+
