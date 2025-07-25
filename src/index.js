03cd2d0b4814afd517a1dd245cf466c0bd8a67e1
Perry Huang <perryhuang2015@gmail.com>
2025-07-25 18:10:26 -0400

    Add checking for localStorage

--- src/index.js
+++ src/index.js
@@ -33,6 +33,31 @@ class Task {
     }
 }
 
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        const x = "__storage_test__";
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    } catch (e) {
+        return (
+            e instanceof DOMException &&
+            e.name === "QuotaExceededError" &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            storage &&
+            storage.length !== 0
+        );
+    }
+}
+
+if (storageAvailable("localStorage")) {
+    console.log("Local storage available");
+} else {
+    console.log("Local storage unavailable");
+}
+
 const projectList = new ProjectList();
 
 projectList.addProject("Chores");
@@ -48,3 +73,5 @@ projectList.projects[2].addTask("Lock in", "Do some work", "2025-07-22", "High")
 
 dom.updateSidebar(projectList);
 dom.showAll(projectList);
+
+console.log(JSON.stringify(projectList));
